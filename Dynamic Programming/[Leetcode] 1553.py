172c9534019f818a982200fbf58edeabdadc98c4
CorradoColaleo <ingegnerecolaleo@gmail.com>
2024-12-22 18:35:22 +0100

    upload leetcode 1553 dp

---
+++ Dynamic Programming/[Leetcode] 1553.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minDays(self, n: int) -> int:
+        memo = {}
+        for i in range(1, n + 1):
+            if i == 1:
+                memo[i] = 1
+            elif i == 2:
+                memo[i] = 2
+            elif i == 3:
+                memo[i] = 2
+            else:
+                if (i % 2) == 0 and (i % 3) == 0:
+                    memo[i] = min(
+                        1 + memo[i - 1],
+                        1 + memo[i - (i // 2)],
+                        1 + memo[i - (2 * (i // 3))]
+                    )
+                elif (i % 2) == 0 and (i % 3) != 0:
+                    memo[i] = min(
+                        1 + memo[i - 1],
+                        1 + memo[i - (i // 2)],
+                    )
+                elif (i % 2) != 0 and (i % 3) == 0:
+                    memo[i] = min(
+                        1 + memo[i - 1],
+                        1 + memo[i - (2 * (i // 3))]
+                    )
+                else:
+                    memo[i] = 1 + memo[i - 1]
+        return memo[n]
